7baeec75a382342b1b6308e2beff71a02a0c1028
pranjjalvarade <pranjjalvarade@gmail.com>
2025-11-04 22:25:32 +0530

    Prints numbers from 1 to 10

---
+++ while_loop2.c
@@ -0,0 +1,14 @@
+//       DISPLAY NUMBERS FROM 1 TO 10
+
+#include <stdio.h>
+int main()
+{
+    int nums = 1;
+
+    while (nums <= 10)
+    {
+        printf("%d\n", nums);
+        nums++;
+    }
+    return 0;
+}

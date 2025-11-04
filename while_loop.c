24125ee6fdb5515c2f433befcb9609074ef3119d
pranjjalvarade <pranjjalvarade@gmail.com>
2025-11-04 22:17:19 +0530

    Introduction to while loop

---
+++ while_loop.c
@@ -0,0 +1,14 @@
+//         INTRODUCTION TO WHILE LOOP
+
+#include <stdio.h>
+int main()
+{
+    int count = 0;
+
+    while (count < 5)
+    {
+        printf("Hello\n");
+        count = count + 1;
+    }
+    return 0 ;
+}

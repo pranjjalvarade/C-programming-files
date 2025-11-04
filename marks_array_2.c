44a440017691db4e90a12603d054b778f8fbacdb
pranjjalvarade <pranjjalvarade@gmail.com>
2025-11-04 19:50:41 +0530

    Upgraded version of marks_array

--- marks_array_2.c
+++ marks_array_2.c
@@ -5,22 +5,31 @@ void main()
 {
     int marks[5];
     int i = 0;
-    printf("Enter marks of 5 subjects\n" );
+    int total_subs = 5;
+    int maxmarkspersub;
+    printf("Enter maximum marks per subject :");
+    scanf("%d", &maxmarkspersub);
+    int maxtotal = total_subs * maxmarkspersub;
+
+    printf("Enter marks of 5 subjects\n");
 
     for (i = 0; i <= 4; i++)
     {
-        printf("Enter the marks for subejct %d:", i+1);
+        printf("Enter the marks for subejct %d:", i + 1);
         scanf("%d", &marks[i]);
     }
 
-    for (i=0 ; i<5 ; i++)
+    for (i = 0; i < 5; i++)
     {
-    printf("Marks of subject %d are %d\n",i+1, marks[i]);
+        printf("Marks of subject %d are %d\n", i + 1, marks[i]);
     }
 
     int total = 0;
 
     for (i = 0; i <= 4; i++)
         total = total + marks[i];
-        printf("total marks are %d\n", total);    
+    printf("Total marks are %d out of %d\n", total, maxtotal);
+
+    float percentage = ((float)total / maxtotal) * 100;
+    printf("The student has scored %f percentage ", percentage);
 }

81e75a6561b05f4fdf6b14042464882a6a5fa858
pranjjalvarade <pranjjalvarade@gmail.com>
2025-11-05 20:00:44 +0530

    Addition of two numbers

--- addition.c
+++ addition.c
@@ -1,11 +1,20 @@
 //    ADDITION OF TWO NUMBERS
 
-#include<stdio.h>
-int  main (){
-    int x , y ;
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
     printf("Enter 2 numbers here:");
-    scanf("%d \n %D", &x , &y);
+    scanf("%d \n %D", &x, &y);
     int sum = x + y;
     printf("The sum of the given two numbers is :%d", sum);
     return 0;
 }
+//         int x = 5 ;
+//         int y = 3;
+//         int power = pow (x,y);
+//         printf("\n %d", power);
+//         return 0;
+
+// }

37c7e4ac9a2bf08325420d0b56b98a1f7575e8cd
pranjjalvarade <pranjjalvarade@gmail.com>
2025-11-06 10:30:02 +0530

    Prints a triangle

--- triangle.c
+++ triangle.c
@@ -1,16 +1,17 @@
 //     PRINTS TRIANGLE OF THE ENTERED NUMBER OF LINES
 
-#include<stdio.h>
-void main (){
-    int n , r ,c ;
+#include <stdio.h>
+void main()
+{
+    int n, r, c;
     printf("Enter a number here:");
     scanf("%d", &n);
 
-    for (r = 1 ; r <= n ; r++)
+    for (r = 1; r <= n; r++)
     {
-        for (c = 1 ; c <= r ; c++ )
-        // printf("%d", c);
-        printf ("*");
+        for (c = 1; c <= r; c++)
+            // printf("%d", c);
+            printf("*");
         printf("\n");
     }
 }

f288aee6768c05e85a473fda9897a2febfe6633d
pranjjalvarade <pranjjalvarade@gmail.com>
2025-11-08 03:05:37 +0530

    Gives numbers divisible by 3 and not 4

---
+++ di_4_3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Numbers between 1 and %d that are divisible by 3 but not by 4:\n", n);
+
+    for (i = 1; i <= n; i++) 
+    {
+        if (i % 3 == 0 && i % 4 != 0) 
+        {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
